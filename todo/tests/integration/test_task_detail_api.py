0cb0e7ecad3fac86dfb855f1d8aa7d90106117ee
Anuj Chhikara <107175639+AnujChhikara@users.noreply.github.com>
2025-06-07 12:25:49 +0530

    refactor: test_task_detail integration test to use db

--- todo/tests/integration/test_task_detail_api.py
+++ todo/tests/integration/test_task_detail_api.py
@@ -1,76 +1,49 @@
-from unittest.mock import patch
-from rest_framework import status
-from rest_framework.test import APITestCase
-from django.urls import reverse
 from bson import ObjectId
-
-from todo.services.task_service import TaskService
-from todo.constants.messages import ValidationErrors, ApiErrors
-from todo.dto.responses.error_response import ApiErrorSource
-from todo.exceptions.task_exceptions import TaskNotFoundException
-from todo.tests.fixtures.task import task_dtos
-from todo.constants.task import TaskPriority, TaskStatus
+from http import HTTPStatus
+from rest_framework.test import APIClient
+from todo.tests.fixtures.task import tasks_db_data
+from todo.tests.integration.base_mongo_test import BaseMongoTestCase
 
 
-class TaskDetailAPIIntegrationTest(APITestCase):
-    @patch("todo.services.task_service.TaskService.get_task_by_id")
-    def test_get_task_by_id_success(self, mock_get_task_by_id):
-        fixture_task_dto = task_dtos[0]
-        task_id_str = fixture_task_dto.id
+class TaskDetailAPIIntegrationTest(BaseMongoTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.fixture_task = tasks_db_data[1].copy()
+        cls.fixture_task["_id"] = cls.fixture_task.pop("id")
+        cls.db.tasks.insert_one(cls.fixture_task)
+        cls.existing_task_id = str(cls.fixture_task["_id"])
+        cls.non_existent_id = str(ObjectId())
+        cls.invalid_task_id = "invalid-task-id"
+        cls.client = APIClient()
 
-        mock_get_task_by_id.return_value = fixture_task_dto
+    def test_get_task_by_id_success(self):
+        response = self.client.get(f"/v1/tasks/{self.existing_task_id}")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        data = response.json()["data"]
+        self.assertEqual(data["id"], self.existing_task_id)
+        self.assertEqual(data["title"], self.fixture_task["title"])
+        self.assertEqual(data["priority"], "MEDIUM")
+        self.assertEqual(data["status"], self.fixture_task["status"])
+        self.assertEqual(data["displayId"], self.fixture_task["displayId"])
+        self.assertEqual(data["createdBy"]["id"], self.fixture_task["createdBy"])
 
-        url = reverse("task_detail", args=[task_id_str])
-        response = self.client.get(url)
+    def test_get_task_by_id_not_found(self):
+        response = self.client.get(f"/v1/tasks/{self.non_existent_id}")
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        error_message = f"Task with ID {self.non_existent_id} not found."
+        self.assertEqual(data["message"], error_message)
+        error = data["errors"][0]
+        self.assertEqual(error["source"]["path"], "task_id")
+        self.assertEqual(error["title"], "Resource Not Found")
+        self.assertEqual(error["detail"], error_message)
 
-        response_data_outer = response.data
-        response_data_inner = response_data_outer.get("data")
-        self.assertIsNotNone(response_data_inner)
+    def test_get_task_by_id_invalid_format(self):
+        response = self.client.get(f"/v1/tasks/{self.invalid_task_id}")
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
 
-        self.assertEqual(response_data_inner["id"], fixture_task_dto.id)
-        self.assertEqual(response_data_inner["title"], fixture_task_dto.title)
-
-        self.assertEqual(response_data_inner["priority"], TaskPriority(fixture_task_dto.priority).name)
-        self.assertEqual(response_data_inner["status"], TaskStatus(fixture_task_dto.status).value)
-
-        self.assertEqual(response_data_inner["displayId"], fixture_task_dto.displayId)
-
-        if fixture_task_dto.createdBy:
-            self.assertEqual(response_data_inner["createdBy"]["id"], fixture_task_dto.createdBy.id)
-            self.assertEqual(response_data_inner["createdBy"]["name"], fixture_task_dto.createdBy.name)
-
-        mock_get_task_by_id.assert_called_once_with(task_id_str)
-
-    @patch("todo.services.task_service.TaskService.get_task_by_id")
-    def test_get_task_by_id_not_found(self, mock_get_task_by_id):
-        non_existent_id = str(ObjectId())
-        mock_get_task_by_id.side_effect = TaskNotFoundException()
-
-        url = reverse("task_detail", args=[non_existent_id])
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        error_detail = response.data.get("errors", [{}])[0].get("detail")
-        self.assertEqual(error_detail, "Task not found.")
-        mock_get_task_by_id.assert_called_once_with(non_existent_id)
-
-    @patch.object(TaskService, "get_task_by_id", wraps=TaskService.get_task_by_id)
-    def test_get_task_by_id_invalid_format(self, mock_actual_get_task_by_id):
-        invalid_task_id = "invalid-id"
-        url = reverse("task_detail", args=[invalid_task_id])
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data["message"], ValidationErrors.INVALID_TASK_ID_FORMAT)
-        self.assertIsNotNone(response.data.get("errors"))
-        self.assertEqual(len(response.data["errors"]), 1)
-
-        error_obj = response.data["errors"][0]
-        self.assertEqual(error_obj["detail"], ValidationErrors.INVALID_TASK_ID_FORMAT)
-        self.assertIn(ApiErrorSource.PATH.value, error_obj["source"])
-        self.assertEqual(error_obj["source"][ApiErrorSource.PATH.value], "task_id")
-        self.assertEqual(error_obj["title"], ApiErrors.VALIDATION_ERROR)
-
-        mock_actual_get_task_by_id.assert_called_once_with(invalid_task_id)
+        data = response.json()
+        self.assertEqual(data["message"], "Please enter a valid Task ID format.")
+        self.assertEqual(data["errors"][0]["title"], "Validation Error")

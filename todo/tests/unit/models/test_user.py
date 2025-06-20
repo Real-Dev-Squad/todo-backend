eb82b9f941fd93519e7de47975af8a2c27ef6bba
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2025-06-21 03:09:46 +0530

    refactor tests based on the comments of auth test pr

--- todo/tests/unit/models/test_user.py
+++ todo/tests/unit/models/test_user.py
@@ -9,15 +9,6 @@ class UserModelTest(TestCase):
     def setUp(self) -> None:
         self.valid_user_data = users_db_data[0]
 
-    def test_user_model_instantiates_with_valid_data(self):
-        user = UserModel(**self.valid_user_data)
-
-        self.assertEqual(user.google_id, self.valid_user_data["google_id"])
-        self.assertEqual(user.email_id, self.valid_user_data["email_id"])
-        self.assertEqual(user.name, self.valid_user_data["name"])
-        self.assertEqual(user.created_at, self.valid_user_data["created_at"])
-        self.assertEqual(user.updated_at, self.valid_user_data["updated_at"])
-
     def test_user_model_throws_error_when_missing_required_fields(self):
         required_fields = ["google_id", "email_id", "name"]
 
@@ -41,15 +32,3 @@ class UserModelTest(TestCase):
 
         error_fields = [e["loc"][0] for e in context.exception.errors()]
         self.assertIn("email_id", error_fields)
-
-    def test_user_model_sets_default_timestamps(self):
-        minimal_data = {
-            "google_id": self.valid_user_data["google_id"],
-            "email_id": self.valid_user_data["email_id"],
-            "name": self.valid_user_data["name"],
-        }
-        user = UserModel(**minimal_data)
-
-        self.assertIsInstance(user.created_at, datetime)
-        self.assertIsNone(user.updated_at)
-        self.assertLessEqual(user.created_at, datetime.now(timezone.utc))

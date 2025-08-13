cbbc0cbc89feacfbf32254e5431b57d2703a2a88
Amit Prakash <34869115+iamitprakash@users.noreply.github.com>
2025-08-14 02:28:48 +0530

    Watchlist fix (#246)
    * Update watchlist_repository.py
    
    * Update test_watchlist_service.py
    
    * Update watchlist_dto.py
    
    * Update watchlist_repository.py

--- todo/repositories/watchlist_repository.py
+++ todo/repositories/watchlist_repository.py
@@ -70,6 +70,16 @@ class WatchlistRepository(MongoRepository):
                         {"$unwind": "$task"},
                         {
                             "$lookup": {
+                                "from": "users",
+                                "let": {"createdById": "$task.createdBy"},
+                                "pipeline": [
+                                    {"$match": {"$expr": {"$eq": ["$_id", {"$toObjectId": "$$createdById"}]}}}
+                                ],
+                                "as": "created_by_user",
+                            }
+                        },
+                        {
+                            "$lookup": {
                                 "from": "task_details",
                                 "let": {"taskIdStr": "$taskId"},
                                 "pipeline": [
